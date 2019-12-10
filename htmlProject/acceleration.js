a3b0bb0aec2c0ee043dc62a8c1fd03f36c2f19ff
Korbinian <korbinian.rudolf@outlook.de>
2019-12-10 13:38:04 +0100

    start to detect play and pause with orientation

--- htmlProject/acceleration.js
+++ htmlProject/acceleration.js
@@ -1,23 +1,19 @@
 window.ondevicemotion = function(event) {
-    playPause(event);
-};
-
-function playPause(event) {
     const ax = Math.round((event.accelerationIncludingGravity.x));
     const ay = Math.round((event.accelerationIncludingGravity.y));
     const az = Math.round((event.accelerationIncludingGravity.z));
 
-    let output = "play";
-    const z = Math.abs(az);
-    if(!((z > ax) && (z > ay) && (az < 0))) {
-        output = "pause";
-    } else {
-        output = "pause";
-    }
-    document.querySelector("#status").innerHTML = output;
     document.querySelector("#acc").innerHTML = "X = " + ax + "<br>" + "Y = " + ay + "<br>" + "Z = " + az;
-}
+
+};
+
 
 window.addEventListener("deviceorientation", function(event) {
-    document.querySelector("#mag").innerHTML = "alpha = " + event.alpha + "<br>" + "beta = " + event.beta + "<br>" + "gamma = " + event.gamma;
+    const alpha = Math.round(event.alpha);
+    const beta = Math.round(event.beta);
+    const gamma = Math.round(event.beta);
+
+
+
+    document.querySelector("#mag").innerHTML = "alpha = " + alpha + "<br>" + "beta = " + beta + "<br>" + "gamma = " + gamma;
 }, true);
