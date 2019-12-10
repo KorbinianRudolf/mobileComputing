79595034f0225fdf4a70a5a26834cc76b6b29011
Korbinian <korbinian.rudolf@outlook.de>
2019-12-10 11:16:43 +0100

    acceleration without gravity

--- htmlProject/acceleration.js
+++ htmlProject/acceleration.js
@@ -3,6 +3,11 @@ window.ondevicemotion = function(event) {
     var ay = event.acceleration.y;
     var az = event.acceleration.z;
 
+    let output = "pause";
+    if(az > ax && az > ay && az < 0) {
+        output = "play"
+    }
+    document.querySelector("#status").innerHTML = output;
     document.querySelector("#acc").innerHTML = "X = " + ax + "<br>" + "Y = " + ay + "<br>" + "Z = " + az;
 };
 
