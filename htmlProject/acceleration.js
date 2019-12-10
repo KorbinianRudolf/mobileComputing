1dc1f40c381fa871d4720509bf846bd2a79ac5d4
Korbinian <korbinian.rudolf@outlook.de>
2019-12-10 13:29:01 +0100

    own method for play and pause

--- htmlProject/acceleration.js
+++ htmlProject/acceleration.js
@@ -1,18 +1,22 @@
 window.ondevicemotion = function(event) {
-    var ax = event.acceleration.x;
-    var ay = event.acceleration.y;
-    var az = event.acceleration.z;
+    playPause(event);
+};
+
+function playPause(event) {
+    const ax = Math.round((event.accelerationIncludingGravity.x));
+    const ay = Math.round((event.accelerationIncludingGravity.y));
+    const az = Math.round((event.accelerationIncludingGravity.z));
 
     let output = "play";
     const z = Math.abs(az);
-    if(!(z > ax && z > ay && az < 0)) {
+    if(!((z > ax) && (z > ay) && (az < 0))) {
         output = "pause";
     } else {
         output = "pause";
     }
     document.querySelector("#status").innerHTML = output;
     document.querySelector("#acc").innerHTML = "X = " + ax + "<br>" + "Y = " + ay + "<br>" + "Z = " + az;
-};
+}
 
 window.addEventListener("deviceorientation", function(event) {
     document.querySelector("#mag").innerHTML = "alpha = " + event.alpha + "<br>" + "beta = " + event.beta + "<br>" + "gamma = " + event.gamma;
