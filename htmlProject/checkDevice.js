7a765265017e032bf251cae002cda789f43b7f30
Korbinian <korbinian.rudolf@outlook.de>
2020-02-09 13:43:30 +0100

    Device check 2

---
+++ htmlProject/checkDevice.js
@@ -0,0 +1,6 @@
+var isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+
+if(!isMobile) {
+    alert("Hallo, sieht so aus, als würde dieses Gerät kein Gyroskop o.Ä. besitzen. \n" +
+        "Versuchen sie es mal mit einem Handy.");
+}
