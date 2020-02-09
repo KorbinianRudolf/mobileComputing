9e02cd1ecbe9b12efd637d16cae543a94b4fa49c
Korbinian <korbinian.rudolf@outlook.de>
2020-02-09 13:46:42 +0100

    deleted debug message

--- htmlProject/checkDevice.js
+++ htmlProject/checkDevice.js
@@ -1,6 +1,4 @@
 var isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
-print("is called");
-
 if(!isMobile) {
     alert("Hallo, sieht so aus, als würde dieses Gerät kein Gyroskop o.Ä. besitzen. \n" +
         "Versuchen sie es mal mit einem Handy.");
